25e1dd2aa1b942a2a34afe0db95c9af8f3f17e30
angel19951 <2384@holbertonschool.com>
2021-03-12 13:52:56 +0000

    Fixing code for checks and adding functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	right = binary_tree_height(tree->right);
 
 	if (left == right)
-		return (binary_tree_is_perfect(tree->left) == binary_tree_is_perfect(tree->right));
+		return (binary_tree_is_perfect(tree->left) ==
+				binary_tree_is_perfect(tree->right));
 
 	return (0);
 }
